f3ac3cf7740758e29a62413d1354b1dc8d390687
IslomjonDev <mamadiyevislomjon2@gmail.com>
2024-06-07 20:02:40 +0500

    responsiv qilindi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
+import { RxHamburgerMenu } from "react-icons/rx";
+
 const Header = () => {
+
+  let [burger , setBurger] = useState(false)
+
   return (
+
     <>
     <header>
         <div className="container">
@@ -9,13 +15,18 @@ const Header = () => {
                 <div>
                   <h2>Islomjon</h2>
                 </div>
-                <div>
+                <div className={`nav__list ${burger ? 'show' : ''}`}>
                     <ul>
                         <li><a href="">HOME</a></li>
                         <li><a href="">PROJECTS</a></li>
                         <li><a href="">SKILLS</a></li>
                         <li><a href="">CONTACT</a></li>
                     </ul>
+                </div>
+                <div className="burger">
+                  <button onClick={() => setBurger(!burger)}>
+                   <RxHamburgerMenu />
+                  </button>
                 </div>
             </div>
         </div>
